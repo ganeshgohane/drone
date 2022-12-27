eae28750347619a761e3907fc6671d5aee179877
ganesh <ganeshsgohane>
2022-12-27 14:24:46 +0530

    hellow

--- drone/Playbook/myfirstteraform.tf
+++ drone/Playbook/myfirstteraform.tf
@@ -4,6 +4,6 @@
    resource "aws_instance" "web-server" {
      ami = "ami-0a606d8395a538502"
      instance_type = "t2.micro"
-     security_groups = ["default"]
+     security_groups = ["sg-04063eead5c30d326"]
      key_name = "GanesAAA"
  }
