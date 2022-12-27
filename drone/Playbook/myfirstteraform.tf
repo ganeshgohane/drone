6b2bb88de1cc88d8613619d9d87f4668e1ae747c
ganesh <ganeshsgohane>
2022-12-27 14:32:53 +0530

    hellow

--- drone/Playbook/myfirstteraform.tf
+++ drone/Playbook/myfirstteraform.tf
@@ -1,10 +1,10 @@
    provider "aws" {
     region = "us-east-2"
 }
-   resource "aws_instance" "web-server" {
+   resource "aws_instance" "GANESH" {
      ami = "ami-0a606d8395a538502"
      instance_type = "t2.micro"
      security_groups = ["sg-04063eead5c30d326"]
      key_name = "GanesAAA"
  }
- 
+
