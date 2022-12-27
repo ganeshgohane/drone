0476e2a02c25748ecf14ba1497870e99ad147b3b
ganesh <ganeshsgohane>
2022-12-27 15:17:57 +0530

    hellow

--- drone/Playbook/MYvcp.tf
+++ drone/Playbook/MYvcp.tf
@@ -2,8 +2,12 @@ resource "aws_vpc" "mynewvpc" {
   cidr_block       = "172.168.0.0/16"
   instance_tenancy = "default"
 
-
-  tags = {
+ tags = {
     Name = "mynewvpc"
   }
+}
+
+
+provider "aws" {
+  region = "us-east-2"
 }
